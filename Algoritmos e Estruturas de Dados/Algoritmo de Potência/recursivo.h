83d3f9feef1b82ed905f98d32be61048651ed1f4
Mikazukill <gabriel.verissimo7684@gmail.com>
2026-04-16 15:47:05 +0000

    Ajustado código de aula função recursiva

--- Algoritmos e Estruturas de Dados/Algoritmo de Potência/recursivo.h
+++ Algoritmos e Estruturas de Dados/Algoritmo de Potência/recursivo.h
@@ -1,25 +1,14 @@
-int recursiva (int base, int exp) {
+int recursiva(int base, int exp) {
 
     if (exp > 0 && base > 0) {
-
         if (exp == 0) {
-
             return 1;
-
         } else if (exp == 1) {
-
                 return base;
-
             } else {
-
-                return pow (base, exp - 1)* base;
-
+                return recursiva(base, exp - 1)* base;
             }
-
     } else {
-
         return -1;
-
     }
-
 }
