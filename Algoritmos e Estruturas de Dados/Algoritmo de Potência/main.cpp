93ec2c9b8430c4c07454a4b822d813ca4eb63e97
Mikazukill <gabriel.verissimo7684@gmail.com>
2026-04-23 18:28:11 +0000

    ajustes recursiva/não recursiva main

--- Algoritmos e Estruturas de Dados/Algoritmo de Potência/main.cpp
+++ Algoritmos e Estruturas de Dados/Algoritmo de Potência/main.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 int main () {
 
-int a = 5;
-int b = 4;
-int resposta = recursiva(a,b);
-cout << "resultado: " << resposta << endl;
+    int a = 5;
+    int b = 4;
+    
+    int resposta = recursiva(a,b);
+
+    cout << "resultado: " << resposta << endl;
 
 return 0;
 }
