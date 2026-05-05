e37fb5e120fce7318afd3dc1fd503a2420b7ea6d
Mikazukill <gabriel.verissimo7684@gmail.com>
2026-05-05 20:21:36 +0000

    ajustes

--- Algoritmos e Estruturas de Dados/Trabalho T1/programa3/calculoSimples.cpp
+++ Algoritmos e Estruturas de Dados/Trabalho T1/programa3/calculoSimples.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main () {
 
-    int id;
-    double preco;
-    int quantia;
+    int id1;
+    double preco1;
+    int quantia1;
 
-    cin >> id;
-    cin >> preco;
-    cin >> quantia;
+    int id2;
+    double preco2;
+    int quantia2;
 
-    double valor = quantia * preco;
+    cin >> id1;
+    cin >> preco1;
+    cin >> quantia1;
 
-    cout << "" << valor << endl;
+    cin >> id2;
+    cin >> preco2;
+    cin >> quantia2;
+
+    double valor1 = quantia1 * preco1;
+    double valor2 = quantia2 * preco2;
+    double total = valor1 + valor2;
+
+    cout << "" << total << endl;
     
     return 0;
 }
